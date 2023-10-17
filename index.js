607b3d932d8539627cec243848851969d92f4f87
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-10-17 10:31:27 -0500

    clase 19 estable

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const routerApi = require('./routes/index.routes');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -14,27 +16,10 @@ app.get('/nueva-ruta', (req, res) => {
 
 app.use(express.json())
 routerApi(app);
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
-
-// app.get('/users', (req, res) => {
-//   const { limit, offset } = req.query;
-//   if (limit && offset) {
-//     res.json({
-//       limit,
-//       offset
-//     });
-//   } else {
-//     res.send('No hay parametros');
-//   }
-// });
-
-// app.get('/categories/:categoryId/products/:productId', (req, res) => {
-//   const { categoryId, productId } = req.params;
-//   res.json({
-//     categoryId,
-//     productId,
-//   });
-// })
 
 app.listen(port, () => {
   console.log('Mi port' +  port);
