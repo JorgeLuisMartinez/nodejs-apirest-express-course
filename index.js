60607cbc79ed10426b5c9d7c213b5b39b882d312
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-10-18 15:36:47 -0500

    final charge

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 
 const routerApi = require('./routes/index.routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+const whiteList = ['http://localhost:8080/', 'http://myapp.co/' ];
+const options = {
+  origin: (origin, callback)=>{
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -14,7 +29,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.use(express.json())
+
 routerApi(app);
 app.use(logErrors);
 app.use(boomErrorHandler);
